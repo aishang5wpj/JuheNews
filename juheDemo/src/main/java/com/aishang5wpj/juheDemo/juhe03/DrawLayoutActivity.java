6bc9cb8e0f918ea75ff65d53d01315198ee82258
aishang5wpj <337487365@qq.com>
2016-05-31 09:21:11 +0800

    解决Button字母默认变大的问题

---
+++ juheDemo/src/main/java/com/aishang5wpj/juheDemo/juhe03/DrawLayoutActivity.java
@@ -0,0 +1,19 @@
+package com.aishang5wpj.juheDemo.juhe03;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.aishang5wpj.juheDemo.R;
+
+/**
+ * Created by wpj on 16/5/30下午3:32.
+ */
+public class DrawLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_draw_layout);
+    }
+}
